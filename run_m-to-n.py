044904024e64ee24d9ad1fcd972d09f331a49d18
Matt Graber <grabermtw@gmail.com>
2023-03-05 16:58:35 -0500

    Added logic to skip 1-to-1 EWE

--- run_m-to-n.py
+++ run_m-to-n.py
@@ -10,17 +10,19 @@ for dataset, num_class in datasets:
     # iterate over the different values of m and n
     for m in range(1,max_m):
         for n in range(1,max_n):
-            # iterate over the different positions of m and n
-            m_combos = list(itertools.combinations(range(num_class), m))
-            n_combos = list(itertools.combinations(range(num_class), n))
-            outfile = os.path.join("results", dataset + "_" + str(m) + "_" + str(n) + ".csv")
-            if not os.path.isfile(outfile):
-                os.makedirs(os.path.dirname(outfile), exist_ok=True)
-                with open(outfile, "w+") as f:
-                    f.write("Sources,Targets,Victim Accuracy,Victim WM Success Rate,Extracted Accuracy,Extracted WM Success Rate,Baseline Accuracy,Baseline WM Success Rate")
-            for m_combo in m_combos:
-                for n_combo in n_combos:
-                    cmd = "python3 train.py --dataset {0} --source {1} --target {2} --outfile {3}".format(dataset, list(m_combo), list(n_combo), outfile)
-                    print("Running command:", cmd)
-                    subprocess.run(cmd, shell=True)
+            # skip 1-to-1 watermarking because the original authors already investigated that
+            if not (m == n and m == 1):
+                # iterate over the different positions of m and n
+                m_combos = list(itertools.combinations(range(num_class), m))
+                n_combos = list(itertools.combinations(range(num_class), n))
+                outfile = os.path.join("results", dataset + "_" + str(m) + "_" + str(n) + ".csv")
+                if not os.path.isfile(outfile):
+                    os.makedirs(os.path.dirname(outfile), exist_ok=True)
+                    with open(outfile, "w+") as f:
+                        f.write("Sources,Targets,Victim Accuracy,Victim WM Success Rate,Extracted Accuracy,Extracted WM Success Rate,Baseline Accuracy,Baseline WM Success Rate")
+                for m_combo in m_combos:
+                    for n_combo in n_combos:
+                        cmd = "python3 train.py --dataset {0} --source {1} --target {2} --outfile {3}".format(dataset, list(m_combo), list(n_combo), outfile)
+                        print("Running command:", cmd)
+                        subprocess.run(cmd, shell=True)
                     
